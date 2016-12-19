6a35aa561d27af63a7aef5384ad0b13461eeb382
Kenta Kida <mutugorou603@gmail.com>
2016-12-19 15:55:47 +0900

    first commit

---
+++ ER101.php
@@ -0,0 +1,17 @@
+<html>
+  <head>
+    <title>Chat-Error101</title>
+
+  </head>
+  <body>
+  	<h1>Chat</h1>
+  	<font color=red>
+  	<h2>Error</h2>
+  	"Please input your id and password.
+  	</font>
+  	<form action="WC101.php">
+  	<input type="submit" name="back" value="back">
+  	</form>
+  </body>
+</html>
+
---
+++ WC101.php
@@ -0,0 +1,20 @@
+<html>
+  <head>
+    <title>Chat-Login</title>
+
+  </head>
+  <body>
+    <form action="WC201.php">
+  	<h1>Chat</h1>
+    <p>Login ID 
+    <input type="text" name="inputValue" value=""></p>
+    <input type="hidden" name="mode" value="login">
+    <p>Password
+    <input type="text" name="password" value=""></p>
+    <input type="hidden" name="mode" value="login">
+    <input type="submit" name="name" value="Login"style="position: absolute; left: 10%;">
+	
+  </form>
+  </body>
+</html>
+
